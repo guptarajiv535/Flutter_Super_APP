414bf121bae5c1d8c03b2c9566d121b862b8bdc2
“guptarajiv535” <rajiv.gupta@techbulls.com>
2023-10-20 12:01:12 +0530

    Added UI related components in both module.

--- task_manager_module/lib/task_manager_module.dart
+++ task_manager_module/lib/task_manager_module.dart
@@ -10,9 +10,63 @@ class TaskManagerScreenView extends StatelessWidget {
         title: Text('Task Manager Module'),
       ),
       body: Center(
-        child: Text('Content of Task Manager Module'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Icon(
+                Icons.assignment,
+                size: 100,
+                color: Colors.blue,
+              ),
+              SizedBox(height: 20),
+              const Text(
+                'My Tasks',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TaskCard(taskTitle: 'Task 1', taskDescription: 'Complete assignment by 5 PM'),
+              TaskCard(taskTitle: 'Task 2', taskDescription: 'Meeting at 3 PM'),
+              // Add more TaskCard widgets as needed
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Add logic to add a new task
+                },
+                child: const Text('Add Task'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class TaskCard extends StatelessWidget {
+  final String taskTitle;
+  final String taskDescription;
+
+  TaskCard({required this.taskTitle, required this.taskDescription});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: Text(taskTitle, style: TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(taskDescription),
+        trailing: IconButton(
+          icon: const Icon(Icons.delete),
+          onPressed: () {
+            // Add logic to delete the task
+          },
+        ),
+      ),
+    );
+  }
+}
+
 
--- weather_module/lib/weather_module.dart
+++ weather_module/lib/weather_module.dart
@@ -7,12 +7,50 @@ class WeatherScreenView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather Module'),
+        title: const Text('Weather Module'),
       ),
       body: Center(
-        child: Text('Content of Weather Module'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            /*Image.asset(
+              'assets/weather_icon.png',
+              width: 100,
+              height: 100,
+            ),*/
+            const SizedBox(height: 20),
+            const Text(
+              'Current Weather',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Sunny with clear skies', // Replace this with actual weather description
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.cloud, size: 30, color: Colors.blue),
+                SizedBox(width: 10),
+                Text(
+                  '25°C', // Replace this with actual temperature
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add logic to fetch and display updated weather information
+              },
+              child: const Text('Refresh Weather'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
 
